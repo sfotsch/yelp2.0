47eba94edad891ff1eff96ebe4274b6627df8a42
shawnfotsch <sfotsch@nd.edu>
2016-12-04 11:46:43 -0500

    add responsive useful buttons

--- review-script.js
+++ review-script.js
@@ -7,4 +7,32 @@ window.onload = function(e){
 		document.getElementById("submitted-review").style.display="block";
 	}
 
+	document.getElementById("u1").onclick = function(){
+			document.getElementById("u1").style.backgroundColor = "gray";
+	}
+
+	document.getElementById("u2").onclick = function(){
+			document.getElementById("u1").style.backgroundColor = "gray";
+	}
+		document.getElementById("u2").onclick = function(){
+			document.getElementById("u1").style.backgroundColor = "gray";
+	}
+
+		document.getElementById("u3").onclick = function(){
+			document.getElementById("u3").style.backgroundColor = "gray";
+	}
+
+		document.getElementById("n1").onclick = function(){
+			document.getElementById("n1").style.backgroundColor = "gray";
+	}
+
+		document.getElementById("n2").onclick = function(){
+			document.getElementById("n2").style.backgroundColor = "gray";
+	}
+
+		document.getElementById("n3").onclick = function(){
+			document.getElementById("n3").style.backgroundColor = "gray";
+	}
+
+
 }
